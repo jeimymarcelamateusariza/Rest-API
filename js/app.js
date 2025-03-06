8b2f0537923c4f76cd62c96381bd4269d2ed6267
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-03-06 11:28:00 -0500

    Validando formulario modal

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+}
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente(){
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    //Revisar si hay campos vacios
+    const camposVacios = [mesa, hora].some(campo => campo ==='');
+
+    if(camposVacios){
+        const existeAlerta = document.querySelector('.invalid-feedback')
+        if(!existeAlerta){
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('d-block', 'text-center', 'invalid-feedback');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove()
+            }, 3000);
+        }return;
+        }
+        console.log('Formulario completo');
+}
